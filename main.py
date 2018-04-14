68181e3fc127c4765a7d141b0ea1ffe44d5f6689
Oradle <d.bogdan99@gmail.com>
2018-04-15 02:14:02 +0300

    First commit
    First commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import sys
+import imaplib
+import email
+import email.header
+import datetime
+
+
+EMAIL_ACCOUNT = "tef.tr62@gmail.com"
+EMAIL_FOLDER = "INBOX"
+
+
+def process_mailbox(M):
+    rv, data = M.search(None, "ALL")
+    if rv != 'OK':
+        print("No messages found!")
+        return
+
+    for num in data[0].split():
+        rv, data = M.fetch(num, '(RFC822)')
+        if rv != 'OK':
+            print("ERROR getting message", num)
+            return
+
+        msg = email.message_from_bytes(data[0][1])
+        hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
+        subject = str(hdr)
+        print('Message %s: %s' % (num, subject))
+        print('Raw Date:', msg['Date'])
+        # Now convert to local date-time
+        date_tuple = email.utils.parsedate_tz(msg['Date'])
+        if date_tuple:
+            local_date = datetime.datetime.fromtimestamp(
+                email.utils.mktime_tz(date_tuple))
+            print ("Local Date:", \
+                local_date.strftime("%a, %d %b %Y %H:%M:%S"))
+
+
+M = imaplib.IMAP4_SSL('imap.gmail.com')
+
+try:
+    print("Enter password: ")
+    rv, data = M.login(EMAIL_ACCOUNT, sys.stdin.readline().rstrip())
+except imaplib.IMAP4.error:
+    print ("LOGIN FAILED!!! ")
+    sys.exit(1)
+
+print(rv, data)
+
+rv, mailboxes = M.list()
+if rv == 'OK':
+    print("Mailboxes:")
+    print(mailboxes)
+
+rv, data = M.select(EMAIL_FOLDER)
+if rv == 'OK':
+    print("Processing mailbox...\n")
+    process_mailbox(M)
+    M.close()
+else:
+    print("ERROR: Unable to open mailbox ", rv)
+
+M.logout()
