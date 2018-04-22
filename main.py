f06fde45e8ad2e327219a0c0dbbfe7f0f85c84f7
Oradle <d.bogdan99@gmail.com>
2018-04-22 03:12:11 +0300

    Now script filtering emails, get body of emails and format it to good form
    Now script filtering emails from all to teacher email and home assignment subject, get body of emails and decode it from quoted-printable and format it to good form (removing unnecessary spaces and line breaks, removing unnecessary beginning and closing). Refactored code

--- main.py
+++ main.py
@@ -3,60 +3,90 @@ import imaplib
 import email
 import email.header
 import datetime
+import quopri
 
 
 EMAIL_ACCOUNT = "tef.tr62@gmail.com"
-EMAIL_FOLDER = "INBOX"
 
 
-def process_mailbox(M):
-    rv, data = M.search(None, "ALL")
-    if rv != 'OK':
+def process_mailbox(imapmail):
+    result, data = imapmail.search(None, '(FROM "lis.kostiantyn@gmail.com" SUBJECT "Home assignment")')
+    if result != 'OK':
         print("No messages found!")
         return
 
     for num in data[0].split():
-        rv, data = M.fetch(num, '(RFC822)')
-        if rv != 'OK':
-            print("ERROR getting message", num)
-            return
+        result, data = imapmail.fetch(num, '(RFC822)')
+        if result != 'OK':
+            print("\n\nERROR: getting message\n\n", num)
+            continue
 
-        msg = email.message_from_bytes(data[0][1])
-        hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
-        subject = str(hdr)
+        try:
+            msg = email.message_from_bytes(data[0][1])
+        except Exception as e:
+            print("\n\nERROR: making message\n\n")
+            continue
+
+        try:
+            hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
+            subject = str(hdr)
+        except Exception as e:
+            print("\n\nWARNING: making header\n")
+            subject = "nil"
+
         print('Message %s: %s' % (num, subject))
         print('Raw Date:', msg['Date'])
-        # Now convert to local date-time
+
         date_tuple = email.utils.parsedate_tz(msg['Date'])
         if date_tuple:
-            local_date = datetime.datetime.fromtimestamp(
-                email.utils.mktime_tz(date_tuple))
-            print ("Local Date:", \
-                local_date.strftime("%a, %d %b %Y %H:%M:%S"))
+            local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
+            print("Local Date:", local_date.strftime("%a, %d %b %Y %H:%M:%S"))
+
+        if msg.is_multipart():
+            body = ""
+            for payload in msg.get_payload():
+                body += payload.get_payload()
+        else:
+            body = msg.get_payload()
+
+        body = quopri.decodestring(body)
+        body = body.decode('windows-1251')
+
+        if body.startswith("Здравствуйте, ."):
+            body = body.partition("Здравствуйте, .")
+            body = body[2].strip()
+        else:
+            body = body.strip()
+
+        if body.endswith("mailto:lis.kostiantyn@gmail.com"):
+            body = body.partition("--")
+            body = body[0].strip()
+        else:
+            body = body.strip()
+
+        if body.find("All the materials are here") != -1:
+            body = body.partition("All the materials are here")
+            body = body[0].strip() + '\n\n' + body[1] + body[2]
+
+        print("Body:\n" + body + '\n')
 
 
-M = imaplib.IMAP4_SSL('imap.gmail.com')
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
 
 try:
     print("Enter password: ")
-    rv, data = M.login(EMAIL_ACCOUNT, sys.stdin.readline().rstrip())
+    result, data = mail.login(EMAIL_ACCOUNT, sys.stdin.readline().rstrip())
 except imaplib.IMAP4.error:
-    print ("LOGIN FAILED!!! ")
+    print("LOGIN FAILED!!! ")
     sys.exit(1)
 
-print(rv, data)
+print(result, data)
 
-rv, mailboxes = M.list()
-if rv == 'OK':
-    print("Mailboxes:")
-    print(mailboxes)
+mail.list()
+mail.select("INBOX")
 
-rv, data = M.select(EMAIL_FOLDER)
-if rv == 'OK':
-    print("Processing mailbox...\n")
-    process_mailbox(M)
-    M.close()
-else:
-    print("ERROR: Unable to open mailbox ", rv)
+print("Processing mailbox...\n")
+process_mailbox(mail)
 
-M.logout()
+mail.close()
+mail.logout()
